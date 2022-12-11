03b84d98dbf553ee0f25e9dd67a9060670f34af4
Dmytro <ryliov.work@gmail.com>
2022-12-11 07:27:23 +0100

    style: changing eslint conf, added prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,41 @@
-const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 const HtmlMinimizerWebpackPlugin = require("html-minimizer-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
-const ESLintWebpackPlugin = require('eslint-webpack-plugin');
-
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
 
 //getting mode, development or production
-const mode = process.env.NODE_ENV === 'development'
+const mode = process.env.NODE_ENV === "development";
 
 module.exports = {
   //file i/o settings
-  entry: path.resolve(__dirname, 'src/index.tsx'),
+  entry: path.resolve(__dirname, "src/index.tsx"),
   output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: "/"
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.css', '.scss', '.sass']
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".css", ".scss", ".sass"],
   },
 
   //webpack settings
-  mode: mode ? 'development' : 'production',
+  mode: mode ? "development" : "production",
   optimization: {
     minimize: true,
     minimizer: [
       new TerserWebpackPlugin({
         exclude: /node_modules/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         parallel: true,
       }),
-      new CssMinimizerWebpackPlugin()
-    ]
+      new CssMinimizerWebpackPlugin(),
+    ],
   },
-  devtool: mode ? 'eval-source-map' : 'nosources-source-map',
+  devtool: mode ? "eval-source-map" : "nosources-source-map",
 
   //dev server
   devServer: {
@@ -47,39 +46,38 @@ module.exports = {
   //loaders
   module: {
     rules: [
-
       //scss loaders
       {
         test: /.s[ac]ss/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
 
       //js + jsx loaders
       {
         test: /.js$/i,
         exclude: /node_modules/,
-        include: path.resolve(__dirname, 'src'),
-        use: 'babel-loader',
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
       },
       {
         test: /.jsx$/i,
         exclude: /node_modules/,
-        include: path.resolve(__dirname, 'src'),
-        use: 'babel-loader',
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
       },
 
       //ts + tsx loaders
       {
         test: /.ts$/i,
         exclude: /node_modules/,
-        include: path.resolve(__dirname, 'src'),
-        use: 'babel-loader'
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
       },
       {
         test: /.tsx$/i,
         exclude: /node_modules/,
-        include: path.resolve(__dirname, 'src'),
-        use: 'babel-loader'
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
       },
 
       //image loader
@@ -87,11 +85,11 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
           },
         ],
       },
-    ]
+    ],
   },
 
   // plugins
@@ -102,50 +100,51 @@ module.exports = {
         {},
         {
           inject: true,
-          template: path.resolve(__dirname, 'src/index.html'),
+          template: path.resolve(__dirname, "src/index.html"),
         },
 
         //Only for production
-        mode ? undefined :
-          {
-            minify: {
-              removeComments: true,
-              collapseWhitespace: true,
-              removeRedundantAttributes: true,
-              useShortDoctype: true,
-              removeEmptyAttributes: true,
-              removeStyleLinkTypeAttributes: true,
-              keepClosingSlash: true,
-              minifyJS: true,
-              minifyCSS: true,
-              minifyURLs: true
-            }
-          }
-      )
+        mode
+          ? undefined
+          : {
+              minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+              },
+            },
+      ),
     ),
     new CssMinimizerWebpackPlugin({
       minimizerOptions: {
         presets: [
-          'default',
+          "default",
           {
             discardComments: {
-              removeAll: true
-            }
-          }
-        ]
-      }
+              removeAll: true,
+            },
+          },
+        ],
+      },
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/assets'),
-          to: path.resolve(__dirname, 'dist/assets')
-        }
-      ]
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, "dist/assets"),
+        },
+      ],
     }),
     new HtmlMinimizerWebpackPlugin(),
     new ESLintWebpackPlugin({
-      extensions: ['.js', '.jsx', '.ts', '.tsx',]
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
     }),
-  ]
-}
+  ],
+};
